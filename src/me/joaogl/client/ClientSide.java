ceed84d7f67cdab8123d86324240e28fba2591f2
joaogl <joaoguerralourenco@hotmail.com>
2013-06-14 21:18:06 +0100

    Changes to improve the socket communication. Unsuccessful...

--- src/me/joaogl/client/ClientSide.java
+++ src/me/joaogl/client/ClientSide.java
@@ -4,51 +4,59 @@ import java.io.*;
 import java.net.*;
 
 public class ClientSide {
-    public static void main(String[] args) throws IOException {
 
-    	System.out.println("Connecting to the Aircaft Management Server...");
-    	
-        Socket Socket = null;
-        PrintWriter out = null;
-        BufferedReader in = null;
-        
-    	System.out.println("10%");
+	private static boolean running = false;
+	private static Socket Socket = null;
+	private static PrintWriter out = null;
+	private static BufferedReader in = null;
+	private static BufferedReader fromUser = null;
 
-        try {
-        	System.out.println("15%");
-        	Socket = new Socket("localhost", 24467);
-            out = new PrintWriter(Socket.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(Socket.getInputStream()));
-        	System.out.println("50%");
-        } catch (UnknownHostException e) {
-            System.err.println("Server not available.");
-            System.exit(1);
-        } catch (IOException e) {
-            System.err.println("Server not available.");
-            System.exit(1);
-        }
+	public static void main(String[] args) throws IOException {
+		System.out.println("Connecting to the Aircaft Management Server...");
 
-    	System.out.println("55%");
-        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-        String fromServer;
-        String fromUser;
-    	System.out.println("90%");
-    	System.out.println("100% - Done");
+		try {
+			Socket = new Socket("192.168.1.65", 24467);
+			out = new PrintWriter(Socket.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(Socket.getInputStream()));
+			fromUser = new BufferedReader(new InputStreamReader(System.in));
 
-        while ((fromServer = in.readLine()) != null) {
-            System.out.println("Server: " + fromServer);
-            if (fromServer.equals("Disconnected. Thank you for flying with us, see you soon."))
-                break;
-		    
-            fromUser = stdIn.readLine();
-	    if (fromUser != null) {
-                out.println(fromUser);
-	    }
-        }
+			running = true;
+		} catch (UnknownHostException e) {
+			forceClose();
+		} catch (IOException e) {
+			forceClose();
+		}
 
-        out.close();
-        in.close();
-        stdIn.close();
-        Socket.close();
-    }
+		while (running) {
+			if (in.readLine().equals("disc")) close();
+			if (fromUser.readLine() != null) out.println(fromUser.readLine());
+
+			System.out.println("Server: " + in.readLine());
+		}
+
+		closeSockets();
+	}
+
+	private static void close() {
+		System.out.println("Disconnecting...");
+		running = false;
+	}
+
+	private static void forceClose() {
+		System.err.println("Server not available.");
+		System.exit(1);
+	}
+
+	private static void closeSockets() {
+		try {
+			System.out.println("Program closing.");
+			out.close();
+			in.close();
+			fromUser.close();
+			Socket.close();
+			System.out.println("Program closed.");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
--- src/me/joaogl/server/IncommingManager.java
+++ src/me/joaogl/server/IncommingManager.java
@@ -5,60 +5,55 @@ import me.joaogl.server.aircraftmanager.AircraftManager;
 public class IncommingManager {
 	int stage = 0;
 
-	public String processInput(String in) {
+	public void processInput(String in) {
 		String out = "Empty command";
-		if (in != null) {
-			out = "Unknown command - " + in;
-			if (in.equalsIgnoreCase("start") && stage == 0) {
-				stage = 1;
-				return "Starting a new flight, tell me the aircraft registration.";
-			} else if (in.equalsIgnoreCase("start")) return "You've already started a flight.";
-			if (stage == 1) {
-				if (check(in)) return cancel();
-				else {
-					if (in != null) {
-						if (isInt(in)) {
-							if (AircraftManager.getReg(Integer.parseInt(in)) != null) {
-								int id = AircraftManager.getId(AircraftManager.getReg(Integer.parseInt(in)));
-								stage = 2;
-								return "Thank you, aircraft requested is " + AircraftManager.getReg(id) + " registred with the id " + id + " which fuel available is " + AircraftManager.getFuel(id) + " and aircraft life is " + AircraftManager.getLife(id) + ".";
-							} else return "Type list to get all Planes registrations";
-						} else {
-							if (AircraftManager.getId(in) != DataManager.getErrorvalue()) {
-								int id = AircraftManager.getId(in);
-								stage = 2;
-								return "Thank you, aircraft requested is " + AircraftManager.getReg(id) + " registred with the id " + id + " which fuel available is " + AircraftManager.getFuel(id) + " and aircraft life is " + AircraftManager.getLife(id) + ".";
-							} else return "Type list to get all Planes registrations, " + AircraftManager.getId(in);
-						}
-					} else return "Type list to get all Planes registrations.";
+		out = "Unknown command - " + in;
+		if (in.equalsIgnoreCase("start") && stage == 0) {
+			stage = 1;
+			ServerThread.sendToClient("Starting a new flight, tell me the aircraft registration.");
+		} else if (in.equalsIgnoreCase("start")) ServerThread.sendToClient("You've already started a flight.");
+		if (stage == 1) {
+			if (check(in)) ServerThread.sendToClient(cancel());
+			else {
+				if (isInt(in)) {
+					if (AircraftManager.getReg(Integer.parseInt(in)) != null) {
+						int id = AircraftManager.getId(AircraftManager.getReg(Integer.parseInt(in)));
+						stage = 2;
+						ServerThread.sendToClient("Thank you, aircraft requested is " + AircraftManager.getReg(id) + " registred with the id " + id + " which fuel available is " + AircraftManager.getFuel(id) + " and aircraft life is " + AircraftManager.getLife(id) + ".");
+					} else ServerThread.sendToClient("Type list to get all Planes registrations");
+				} else {
+					if (AircraftManager.getId(in) != DataManager.getErrorvalue()) {
+						int id = AircraftManager.getId(in);
+						stage = 2;
+						ServerThread.sendToClient("Thank you, aircraft requested is " + AircraftManager.getReg(id) + " registred with the id " + id + " which fuel available is " + AircraftManager.getFuel(id) + " and aircraft life is " + AircraftManager.getLife(id) + ".");
+					} else ServerThread.sendToClient("Type list to get all Planes registrations, " + AircraftManager.getId(in));
 				}
-			}
-			if (stage == 2) if (check(in)) return cancel();
-			if (in.equalsIgnoreCase("Disconnecting")) return "Disconnected. Thank you for flying with us, see you soon.";
-			if (in == "new") return "Connected, Welcome.";
-			if (in.equalsIgnoreCase("list")) {
-				String result = "Aircraft List sent to the server console.";
-				for (int i = 0; i < AircraftManager.aircraftReg.size(); i++) {
-					System.out.println(AircraftManager.getAllPlanes(i));
-				}
-				return result;
-			}
-			if (in.contains("delid")) {
-				String[] args = in.split(" ");
-				if (args[1] != null && isInt(args[1])) {
-					int id = Integer.parseInt(args[1]);
-					if (id <= AircraftManager.getTotal()) {
-						AircraftManager.removeId(id);
-						return "Removed id " + args[1];
-					} else return "Error: invalid ID. Type list";
-				} else return "Error: value typed is not an ID. Type delid (id)";
-			}
-			if (in.equalsIgnoreCase("restart")) {
-				DataManager.setupRegList();
-				return "Restart";
 			}
 		}
-		return out;
+		if (stage == 2) if (check(in)) ServerThread.sendToClient(cancel());
+		if (in.equalsIgnoreCase("Disconnecting") || in.equalsIgnoreCase("disc")) ServerThread.sendToClient("disc");
+		if (in.equalsIgnoreCase("list")) {
+			String result = "Aircraft List sent to the server console.";
+			for (int i = 0; i < AircraftManager.aircraftReg.size(); i++) {
+				System.out.println(AircraftManager.getAllPlanes(i));
+			}
+			ServerThread.sendToClient(result);
+		}
+		if (in.contains("delid")) {
+			String[] args = in.split(" ");
+			if (args[1] != null && isInt(args[1])) {
+				int id = Integer.parseInt(args[1]);
+				if (id <= AircraftManager.getTotal()) {
+					AircraftManager.removeId(id);
+					ServerThread.sendToClient("Removed id " + args[1]);
+				} else ServerThread.sendToClient("Error: invalid ID. Type list");
+			} else ServerThread.sendToClient("Error: value typed is not an ID. Type delid (id)");
+		}
+		if (in.equalsIgnoreCase("restart")) {
+			DataManager.setupRegList();
+			ServerThread.sendToClient("Restart");
+		}
+		ServerThread.sendToClient(out);
 	}
 
 	private boolean check(String in) {
